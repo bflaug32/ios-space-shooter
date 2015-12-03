08c45b25380a006c52e3989e17c1742273ab0b69
BRAD FLAUGHER <bflaug32@Gmail.com>
2015-12-03 11:40:40 -0500

    added imojiSDK podfile

---
+++ AlienConqueror/6014-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  ImojiSDKUI
+//
+//  Created by Brad Flaugher 2105
+//
+
+#ifndef artmoji_Artmoji_Bridging_Header_h
+#define artmoji_Artmoji_Bridging_Header_h
+
+#import "ImojiSDK/ImojiSDK.h"
+
+#endif
